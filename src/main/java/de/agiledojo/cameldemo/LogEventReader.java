651f3e77cc0c6972ac40714f473db0676d401a7f
Christian Fischer <cf@fisch3r.de>
2013-06-29 15:13:33 +0200

    setting up spring test

---
+++ src/main/java/de/agiledojo/cameldemo/LogEventReader.java
@@ -0,0 +1,16 @@
+package de.agiledojo.cameldemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogEventReader {
+
+	public List<String> getMessages() {
+		// TODO Auto-generated method stub
+		return new ArrayList<String>();
+	}
+
+}
--- src/test/java/de/agiledojo/cameldemo/LogEventReaderIT.java
+++ src/test/java/de/agiledojo/cameldemo/LogEventReaderIT.java
@@ -1,11 +1,22 @@
 package de.agiledojo.cameldemo;
 
-import org.junit.Test;
+import static org.fest.assertions.Assertions.*;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring/test-context.xml")
 public class LogEventReaderIT {
+
+	@Autowired
+	private LogEventReader sut;
 
 	@Test
 	public void readingFromLogfilesReturnsListOfMessages() {
-
+		assertThat(sut.getMessages()).isNotEmpty();
 	}
 }
