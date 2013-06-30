09636715139043692dbf799a3bbc53ef5289e5dd
Christian Fischer <cf@fisch3r.de>
2013-06-30 13:18:47 +0200

    adding test for camel route

--- src/main/java/de/agiledojo/cameldemo/LogEventReader.java
+++ src/main/java/de/agiledojo/cameldemo/LogEventReader.java
@@ -3,13 +3,19 @@ package de.agiledojo.cameldemo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.camel.Handler;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogEventReader {
 
+	@Handler
+	public void processMessage(String message) {
+
+	}
+
 	public List<String> getMessages() {
-		// TODO Auto-generated method stub
+
 		return new ArrayList<String>();
 	}
 
--- src/test/java/de/agiledojo/cameldemo/LogEventReaderIT.java
+++ src/test/java/de/agiledojo/cameldemo/LogEventReaderIT.java
@@ -1,22 +1,42 @@
 package de.agiledojo.cameldemo;
 
-import static org.fest.assertions.Assertions.*;
-
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/spring/test-context.xml")
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class LogEventReaderIT {
 
-	@Autowired
-	private LogEventReader sut;
+	@EndpointInject(uri = "mock:result")
+	protected MockEndpoint resultEndpoint;
+
+	@Produce(uri = "direct:start")
+	protected ProducerTemplate template;
 
 	@Test
-	public void readingFromLogfilesReturnsListOfMessages() {
-		assertThat(sut.getMessages()).isNotEmpty();
+	public void testSendMatchingMessage() throws Exception {
+		String expectedBody = "<matched/>";
+
+		resultEndpoint.expectedBodiesReceived(expectedBody);
+
+		template.sendBodyAndHeader(expectedBody, "foo", "bar");
+
+		resultEndpoint.assertIsSatisfied();
 	}
+
+	/*
+	 * @Autowired private LogEventReader sut;
+	 * 
+	 * @Test public void readingFromLogfilesReturnsListOfMessages() {
+	 * assertThat(sut.getMessages()).isNotEmpty(); }
+	 */
 }
