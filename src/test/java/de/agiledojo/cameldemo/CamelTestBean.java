ca4f6fac3d549151711b19b6d0cb3b552198f282
Christian Fischer <cf@fisch3r.de>
2013-07-21 15:04:39 +0200

    extending with test bean

---
+++ src/test/java/de/agiledojo/cameldemo/CamelTestBean.java
@@ -0,0 +1,11 @@
+package de.agiledojo.cameldemo;
+
+import org.apache.camel.Handler;
+
+public class CamelTestBean {
+
+	@Handler
+	public void handle(String body) {
+
+	}
+}
--- src/test/java/de/agiledojo/cameldemo/LogEventReaderIT.java
+++ src/test/java/de/agiledojo/cameldemo/LogEventReaderIT.java
@@ -1,11 +1,10 @@
 package de.agiledojo.cameldemo;
 
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,27 +15,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class LogEventReaderIT {
 
+	@Autowired
+	LoginEventPublisher eventPublisher;
+
 	@EndpointInject(uri = "mock:result")
 	protected MockEndpoint resultEndpoint;
 
-	@Produce(uri = "direct:start")
-	protected ProducerTemplate template;
-
 	@Test
 	public void testSendMatchingMessage() throws Exception {
-		String expectedBody = "<matched/>";
+		eventPublisher.fireLoginEvent();
 
-		resultEndpoint.expectedBodiesReceived(expectedBody);
-
-		template.sendBodyAndHeader(expectedBody, "foo", "bar");
+		resultEndpoint.expectedMessageCount(1);
 
 		resultEndpoint.assertIsSatisfied();
 	}
 
-	/*
-	 * @Autowired private LogEventReader sut;
-	 * 
-	 * @Test public void readingFromLogfilesReturnsListOfMessages() {
-	 * assertThat(sut.getMessages()).isNotEmpty(); }
-	 */
 }
