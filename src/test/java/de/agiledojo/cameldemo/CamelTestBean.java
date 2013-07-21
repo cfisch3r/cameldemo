27b95076676d0aba1931777b12081b40f98b7b46
Christian Fischer <c.fischer@faz.net>
2013-07-21 17:11:25 +0200

    removing redundant annotation

--- src/test/java/de/agiledojo/cameldemo/CamelTestBean.java
+++ src/test/java/de/agiledojo/cameldemo/CamelTestBean.java
@@ -1,12 +1,10 @@
 package de.agiledojo.cameldemo;
 
-import org.apache.camel.Handler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CamelTestBean {
 
-	@Handler
 	public boolean handle(Object body) {
 		return (body instanceof LoginEvent);
 	}
